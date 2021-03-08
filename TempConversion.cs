9e2623e64f40a5512cf0dde830650544316427e9
Steven Shetler <79161583+sgs0011@users.noreply.github.com>
2021-03-07 20:10:50 -0500

    Add files via upload

---
+++ TempConversion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+public class TempConversion
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please enter the temp to be converted to Fahrenheit scale: ");
+
+        double tempInCelsius = double.Parse(Console.ReadLine());
+
+        double tempInFahr = (tempInCelsius * 9 / 5) + 32;
+
+
+        if(tempInFahr <=32)
+        {
+            Console.WriteLine($"Converted in F: {tempInFahr}\n It is very cold today!!");
+
+
+        }
+        else
+        {
+            Console.WriteLine($"Converted in F: {tempInFahr}\n It is not very cold today!!");
+
+        }
+
+    }
+
+
+}
